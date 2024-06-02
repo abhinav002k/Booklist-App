7cc11b734371d1bccca6417042d31269b465f556
Abhinav Kankarwal <142111497+abhinav002k@users.noreply.github.com>
2024-06-02 22:14:20 +0530

    added the reducer hook

--- src/contexts/BookContexts.jsx
+++ src/contexts/BookContexts.jsx
@@ -1,24 +1,18 @@
-import { createContext, useState } from 'react'
+import { createContext, useReducer, useState } from 'react'
 import {v4 as uuid} from 'uuid'
+import { BookReducer } from '../reducers/BookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider= (props)=>{
-    const [books,setBooks]=useState([
+
+      const [books,dispatch]=useReducer(BookReducer,[
         {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
         {title: 'the final empire', author: 'brandon sanderson', id: 2},
-      ]);
-
-      const addBook=(title,author)=>{
-        setBooks([...books,{title:title,author:author,id:uuid()}])
-      };
-
-      const removeBook=(id)=>{
-        setBooks(books.filter((book)=>book.id!=id))
-      };
+      ])
       
       return(
-        <BookContext.Provider value={{books,addBook,removeBook}}> 
+        <BookContext.Provider value={{books,dispatch}}> 
             {props.children}
         </BookContext.Provider>
       )
